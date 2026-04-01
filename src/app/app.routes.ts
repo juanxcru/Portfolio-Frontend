9f58bb65cb27b4ae60af87b0e7494194dadca2c9
juanxcru <dinarjuanc@gmail.com>
2026-04-01 19:13:04 +0700

    error routing: basic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { LandingComponent } from './components/landing/landing.component';
 import { PortfolioBodyComponent } from './portfolio/components/portfolio-body/portfolio-body.component';
 
 export const routes: Routes = [
