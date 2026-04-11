1422c9bdba338d8ad0cd52102126a74fd0d5ac33
juanxcru <dinarjuanc@gmail.com>
2026-04-11 18:19:57 +0700

    loading msgs with time

--- src/app/portfolio/components/loading/loading.component.ts
+++ src/app/portfolio/components/loading/loading.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, effect, input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BaseComponent } from '../base-component.component';
 
@@ -10,8 +10,43 @@ import { BaseComponent } from '../base-component.component';
 })
 export class LoadingComponent extends BaseComponent {
   
-  readonly message = input<string>('');
+  readonly message = input<string[]>(['']);
 
   readonly mode = input<'fullscreen' | 'inline'>('fullscreen');
+
+  protected displayMessage = signal('');
+  private timerId: any;
+
+  constructor() {
+    super();
+    
+    effect(() => {
+      const msgs = this.message();
+      if (msgs && msgs.length > 0) {
+        this.displayMessage.set(msgs[0]);
+        this.startRotation(msgs);
+      }
+    });
+  }
+
+  private startRotation(msgs: string[]) {
+    this.stopRotation();
+    if (msgs.length <= 1) return; 
+
+    let index = 1;
+    this.timerId = setInterval(() => {
+      this.displayMessage.set(msgs[index]);
+      index = (index + 1) % msgs.length;
+    }, 4000); 
+  
+  }
+
+  private stopRotation() {
+    if (this.timerId) clearInterval(this.timerId);
+  }
+
+  ngOnDestroy() {
+    this.stopRotation();
+  }
 
 }
